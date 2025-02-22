d62a7f05fbe229231622bec3c2d019e2a8099e7d
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-22 18:12:42 +0300

    feat: :sparkles: add repo interface

---
+++ internal/domain/repository.go
@@ -0,0 +1,21 @@
+package domain
+
+type ChatLinkRepository interface {
+	// Registers a chat in the repository.
+	RegisterChat(chatID int64) error
+
+	// Deletes a chat from the repository.
+	DeleteChat(chatID int64) error
+
+	// Save a link in the repository.
+	SaveLink(chatID int64, link *Link) error
+
+	// Delete a link from the repository.
+	DeleteLink(chatID int64, link *Link) error
+
+	// Get a list of links from the repository.
+	GetListLinks(chatID int64) ([]*Link, error)
+
+	// Check if the chat exists in the repository.
+	CheckUserExistence(chatID int64) bool
+}
