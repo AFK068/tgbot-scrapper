2247a7918f23acd6bf4236d36ecf3d4a1989ffcc
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-22 18:22:08 +0300

    feat: :sparkles: add auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/AFK068/bot/internal/domain"
+	handler "github.com/AFK068/bot/internal/infrastructure/handlers/scrapper"
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	LinksPathPrefix = "/links"
+	TgChatIDHeader  = "Tg-Chat-Id"
+)
+
+func AuthLinkMiddleware(repo domain.ChatLinkRepository) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			if strings.HasPrefix(ctx.Path(), LinksPathPrefix) {
+				if ok, err := checkAuthLink(ctx, repo); !ok {
+					return err
+				}
+			}
+
+			return next(ctx)
+		}
+	}
+}
+
+func checkAuthLink(ctx echo.Context, repo domain.ChatLinkRepository) (bool, error) {
+	tgChatIDStr := ctx.Request().Header.Get(TgChatIDHeader)
+
+	tgChatID, err := strconv.ParseInt(tgChatIDStr, 10, 64)
+	if err != nil {
+		return false, handler.SendBadRequestResponse(ctx, handler.ErrInvalidRequestBody, handler.ErrDescriptionInvalidBody)
+	}
+
+	// Check that user exists in the repository.
+	if !repo.CheckUserExistence(tgChatID) {
+		return false, handler.SendUnauthorizedResponse(ctx, handler.ErrChatNotExist, handler.ErrDescriptionChatNotExist)
+	}
+
+	return true, nil
+}
