2bdcb9283124fbee2af1b5b7875db38e7a3da4a2
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-22 18:25:22 +0300

    feat: :sparkles: add common scrapper handler

---
+++ internal/infrastructure/handlers/scrapper/common.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"net/http"
+
+	api "github.com/AFK068/bot/internal/api/openapi/scrapper/v1"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	ErrChatAlreadyExist   = "chat_already_exists"
+	ErrChatNotExist       = "chat_not_found"
+	ErrInternalError      = "internal_error"
+	ErrInvalidRequestBody = "invalid_request_body"
+
+	ErrDescriptionChatAlreadyExist = "Chat already exists"
+	ErrDescriptionChatNotExist     = "Chat not found"
+	ErrDescriptionInternalError    = "Internal error"
+	ErrDescriptionInvalidBody      = "Invalid request body"
+
+	ErrLinkNotExist            = "link_not_exist"
+	ErrDescriptionLinkNotExist = "Link not exist"
+)
+
+func SendSuccessResponse(ctx echo.Context, data any) error {
+	return ctx.JSON(http.StatusOK, data)
+}
+
+func SendBadRequestResponse(ctx echo.Context, err, description string) error {
+	return ctx.JSON(http.StatusBadRequest, api.ApiErrorResponse{
+		Description:      aws.String(description),
+		Code:             aws.String("400"),
+		ExceptionMessage: aws.String(err),
+	})
+}
+
+func SendNotFoundResponse(ctx echo.Context, err, description string) error {
+	return ctx.JSON(http.StatusNotFound, api.ApiErrorResponse{
+		Description:      aws.String(description),
+		Code:             aws.String("404"),
+		ExceptionMessage: aws.String(err),
+	})
+}
+
+func SendUnauthorizedResponse(ctx echo.Context, err, description string) error {
+	return ctx.JSON(http.StatusUnauthorized, api.ApiErrorResponse{
+		Description:      aws.String(description),
+		Code:             aws.String("401"),
+		ExceptionMessage: aws.String(err),
+	})
+}
