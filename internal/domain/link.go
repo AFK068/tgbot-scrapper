ef46d05d0158b57ac1003de3e8611901aa81fdd1
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-24 15:26:51 +0300

    refactor: :recycle: link fileds

--- internal/domain/link.go
+++ internal/domain/link.go
@@ -1,8 +1,14 @@
 package domain
 
+var (
+	StackoverflowType = "stackoverflow"
+	GithubType        = "github"
+)
+
 type Link struct {
 	ID      int64
 	URL     string
+	Type    string
 	Tags    []string
 	Filters []string
 }
