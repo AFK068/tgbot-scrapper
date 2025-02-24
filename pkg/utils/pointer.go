d02f20cb8fe31ac20219e327fa98fffbf8db36ea
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-25 01:11:14 +0300

    feat: :sparkles: add new ptr method to utils

--- pkg/utils/pointer.go
+++ pkg/utils/pointer.go
@@ -1,8 +1,17 @@
 package utils
 
-func SlicePtr(s []string) *[]string {
+func SliceStringPtr(s []string) *[]string {
 	if s == nil {
 		empty := []string{}
+		return &empty
+	}
+
+	return &s
+}
+
+func SliceInt64Ptr(s []int64) *[]int64 {
+	if s == nil {
+		empty := []int64{}
 		return &empty
 	}
 
