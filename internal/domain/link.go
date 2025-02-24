45a0925bb5f6a4c13c29c261e7ad7a616180946f
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-24 23:25:35 +0300

    perf: :zap: update link struct

--- internal/domain/link.go
+++ internal/domain/link.go
@@ -1,14 +1,17 @@
 package domain
 
+import "time"
+
 var (
 	StackoverflowType = "stackoverflow"
 	GithubType        = "github"
 )
 
 type Link struct {
-	ID      int64
-	URL     string
-	Type    string
-	Tags    []string
-	Filters []string
+	ID        int64
+	URL       string
+	Type      string
+	Tags      []string
+	Filters   []string
+	AddedTime time.Time
 }
