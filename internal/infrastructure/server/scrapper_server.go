0a437ad553157d8fd4b567151075d42394c632be
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-25 23:37:49 +0300

    feat: :sparkles: add bot server

---
+++ internal/infrastructure/server/scrapper_server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"github.com/AFK068/bot/config"
+	api "github.com/AFK068/bot/internal/api/openapi/scrapper/v1"
+	"github.com/AFK068/bot/internal/application/scrapper"
+	"github.com/AFK068/bot/internal/domain"
+	handler "github.com/AFK068/bot/internal/infrastructure/handler/scrapper"
+	"github.com/AFK068/bot/internal/middleware"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ScrapperServer struct {
+	Config    *config.ScrapperConfig
+	Handler   *handler.ScrapperHandler
+	Repo      domain.ChatLinkRepository
+	Scheduler *scrapper.Scheduler
+	Echo      *echo.Echo
+}
+
+func NewScrapperServer(
+	cfg *config.ScrapperConfig,
+	repo domain.ChatLinkRepository,
+	hd *handler.ScrapperHandler,
+	sd *scrapper.Scheduler,
+) *ScrapperServer {
+	return &ScrapperServer{
+		Echo:      echo.New(),
+		Config:    cfg,
+		Repo:      repo,
+		Handler:   hd,
+		Scheduler: sd,
+	}
+}
+
+func (s *ScrapperServer) Start() error {
+	api.RegisterHandlers(s.Echo, s.Handler)
+
+	// Middleware for checking the user authentication.
+	s.Echo.Use(middleware.AuthLinkMiddleware(s.Repo))
+
+	// Run the scrapper.
+	s.Scheduler.Run(scrapper.DefaultJobDuration)
+
+	return s.Echo.Start(s.Config.Host + ":" + s.Config.Port)
+}
