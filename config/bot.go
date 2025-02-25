3e6c7f58ebb8b271e05d8f43710acb4cb43e26f0
Ivan Gobets <6_6_6_66@mail.ru>
2025-02-25 23:49:34 +0300

    feat: :sparkles: add bot config

---
+++ config/bot.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type BotConfig struct {
+	Token       string `mapstructure:"BOT_TOKEN"`
+	Host        string `mapstructure:"SERVER_HOST"`
+	Port        string `mapstructure:"SERVER_PORT"`
+	ScrapperURL string `mapstructure:"SCRAPPER_URL"`
+}
+
+func NewBotConfig(file string) (*BotConfig, error) {
+	viper.AddConfigPath(file)
+	viper.SetConfigName("bot")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("reading config file: %w", err)
+	}
+
+	config := &BotConfig{}
+	if err := viper.Unmarshal(config); err != nil {
+		return nil, fmt.Errorf("unmarshaling server config: %w", err)
+	}
+
+	return config, nil
+}
