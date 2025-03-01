35371721c151b1a85ec3426598f36067efb7607c
Ivan Gobets <6_6_6_66@mail.ru>
2025-03-02 00:35:42 +0300

    perf: :zap: scrapper client error handling

--- internal/infrastructure/clients/scrapper/common.go
+++ internal/infrastructure/clients/scrapper/common.go
@@ -2,31 +2,33 @@ package scrapper
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	api "github.com/AFK068/bot/internal/api/openapi/scrapper/v1"
+	"github.com/AFK068/bot/internal/domain/apperrors"
 )
 
 func handleResponse(code int, body []byte) error {
 	switch code {
 	case http.StatusOK:
 		return nil
-	case http.StatusBadRequest:
+	case http.StatusBadRequest, http.StatusNotFound, http.StatusUnauthorized:
 		var apiErr api.ApiErrorResponse
 		if err := json.Unmarshal(body, &apiErr); err != nil {
-			return fmt.Errorf("failed to decode error response: %w", err)
+			return &apperrors.ErrorResponse{
+				Code:    code,
+				Message: "failed to decode error response",
+			}
 		}
 
-		return fmt.Errorf("bad request: %s", *apiErr.Description)
-	case http.StatusNotFound:
-		var apiErr api.ApiErrorResponse
-		if err := json.Unmarshal(body, &apiErr); err != nil {
-			return fmt.Errorf("failed to decode error response: %w", err)
+		return &apperrors.ErrorResponse{
+			Code:    code,
+			Message: *apiErr.Description,
 		}
-
-		return fmt.Errorf("not found: %s", *apiErr.Description)
 	default:
-		return fmt.Errorf("unexpected status code: %d", code)
+		return &apperrors.ErrorResponse{
+			Code:    code,
+			Message: "unexpected error",
+		}
 	}
 }
