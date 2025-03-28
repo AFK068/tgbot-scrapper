3639574643d3452efee3bdeaf505a922116d98be
Ivan Gobets <6_6_6_66@mail.ru>
2025-03-28 20:32:15 +0300

    feat: :sparkles: add global config

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Storage   Storage   `yaml:"storage" env-required:"true"`
+	Migration Migration `yaml:"migrations" env-required:"true"`
+}
+
+type Storage struct {
+	Host         string `yaml:"host" env:"POSTGRES_HOST" env-required:"true"`
+	Port         string `yaml:"port" env:"POSTGRES_PORT" env-required:"true"`
+	DatabaseName string `yaml:"database_name" env:"POSTGRES_DATABASE_NAME" env-required:"true"`
+	User         string `yaml:"user" env:"POSTGRES_USER" env-required:"true"`
+	Password     string `yaml:"password" env:"POSTGRES_PASSWORD" env-required:"true"`
+}
+
+type Migration struct {
+	MigrationsPath      string `yaml:"migrations_path" env:"MIGRATIONS_PATH" env-required:"true"`
+	MigrationsTableName string `yaml:"migrations_table_name" env:"MIGRATIONS_TABLE_NAME" env-default:"migrations"`
+}
+
+func NewConfig(filePath string) (*Config, error) {
+	config := &Config{}
+
+	if err := cleanenv.ReadConfig(filePath, config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (cfg *Config) GetPostgresConnectionString() string {
+	return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.Storage.User,
+		cfg.Storage.Password,
+		cfg.Storage.Host,
+		cfg.Storage.Port,
+		cfg.Storage.DatabaseName,
+	)
+}
